879b03f498cdf73d6f284d007c736ce3a38d586c
Kamrul Hasan <kamrul@evidentbd.com>
2025-08-14 15:06:02 +0600

    frontend docker image optimized

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   // Allow common local network IPs and patterns
   allowedDevOrigins: ['*'],
   // basePath: '/frontend', // to use as localhost/frontend
+  output: 'standalone',
 };
 
 export default nextConfig;
